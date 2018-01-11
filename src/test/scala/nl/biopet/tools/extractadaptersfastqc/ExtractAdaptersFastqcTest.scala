1a9e578166e284f5ca8271235a032bb569e7e2e4
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-01-11 11:02:21 +0100

    Fixing compile issues

--- src/test/scala/nl/biopet/tools/extractadaptersfastqc/ExtractAdaptersFastqcTest.scala
+++ src/test/scala/nl/biopet/tools/extractadaptersfastqc/ExtractAdaptersFastqcTest.scala
@@ -1,14 +1,15 @@
 package nl.biopet.tools.extractadaptersfastqc
 
 import nl.biopet.utils.test.tools.ToolTest
+import nl.biopet.utils.tool.ToolCommand
 import org.testng.annotations.Test
 
 class ExtractAdaptersFastqcTest extends ToolTest[Args] {
-  def toolCommand: ToolTemplate.type = ToolTemplate
+  def toolCommand: ToolCommand[Args] = ExtractAdaptersFastqc
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
-      ToolTemplate.main(Array())
+      ExtractAdaptersFastqc.main(Array())
     }
   }
 }
