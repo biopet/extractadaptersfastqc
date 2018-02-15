b4331c6c3c979a8b326a0821ceecff6b49e5e0a1
pjvan_thof <pjrvanthof@gmail.com>
2018-02-15 15:50:13 +0100

    auto format

--- src/test/scala/nl/biopet/tools/extractadaptersfastqc/ExtractAdaptersFastqcTest.scala
+++ src/test/scala/nl/biopet/tools/extractadaptersfastqc/ExtractAdaptersFastqcTest.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2018 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.extractadaptersfastqc
 
 import java.io.File
@@ -22,13 +43,19 @@ class ExtractAdaptersFastqcTest extends ToolTest[Args] {
     adapterOutput.deleteOnExit()
     val contamOutput = File.createTempFile("test.", ".txt")
     contamOutput.deleteOnExit()
-    ExtractAdaptersFastqc.main(Array(
-      "-i", resourcePath("/fastqc_data.txt"),
-      "--knownContamFile", resourcePath("/contaminant_list.txt"),
-      "--knownAdapterFile", resourcePath("/adapter_list.txt"),
-      "--adapterOutputFile", adapterOutput.getAbsolutePath,
-      "--contamsOutputFile", contamOutput.getAbsolutePath
-    ))
+    ExtractAdaptersFastqc.main(
+      Array(
+        "-i",
+        resourcePath("/fastqc_data.txt"),
+        "--knownContamFile",
+        resourcePath("/contaminant_list.txt"),
+        "--knownAdapterFile",
+        resourcePath("/adapter_list.txt"),
+        "--adapterOutputFile",
+        adapterOutput.getAbsolutePath,
+        "--contamsOutputFile",
+        contamOutput.getAbsolutePath
+      ))
 
     getLinesFromFile(adapterOutput) shouldBe List("AGATCGGAAGAG")
     getLinesFromFile(contamOutput) shouldBe List(
